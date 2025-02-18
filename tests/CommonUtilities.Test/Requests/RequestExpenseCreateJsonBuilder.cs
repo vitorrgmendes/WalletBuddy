73622bea8d07cac3dfc73ccc2ff01cc1b73f25c6
Vitor Mendes <vitormendes20@gmail.com>
2025-02-18 19:09:08 -0300

    test: add create expense test

---
+++ tests/CommonUtilities.Test/Requests/RequestExpenseCreateJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using WalletBuddy.Communication.Enums;
+using WalletBuddy.Communication.Requests.Expenses;
+
+namespace CommonUtilities.Test.Requests;
+
+public class RequestExpenseCreateJsonBuilder
+{
+    public static RequestExpenseCreateJson Build()
+    {
+        return new Faker<RequestExpenseCreateJson>()
+                .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+                .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+                .RuleFor(r => r.Date, faker => faker.Date.Past())
+                .RuleFor(r => r.Price, faker => faker.Random.Decimal(min:0, max:2000))
+                .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>());
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Create/CreateExpenseValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using CommonUtilities.Test.Requests;
+using FluentAssertions;
+using WalletBuddy.Application.Services.Expenses.Create;
+
+namespace Validators.Tests.Expenses.Create;
+
+public class CreateExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new CreateExpenseValidator();
+        var request = RequestExpenseCreateJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+}
