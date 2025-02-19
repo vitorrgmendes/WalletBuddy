cb83598979ed2960b94e75a241dbb7fddfeff0dc
Vitor Mendes <vitormendes20@gmail.com>
2025-02-19 10:31:24 -0300

    test: add expenses specific tests

--- tests/Validators.Tests/Expenses/Create/CreateExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Create/CreateExpenseValidatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using CommonUtilities.Test.Requests;
+using System.Runtime.CompilerServices;
 using WalletBuddy.Application.Services.Expenses.Create;
+using WalletBuddy.Communication.Enums;
+using WalletBuddy.Exception;
 
 namespace Validators.Tests.Expenses.Create;
 
@@ -14,5 +17,71 @@ public class CreateExpenseValidatorTests
         var result = validator.Validate(request);
 
         Assert.True(result.IsValid);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetWrongTitleData))]
+    public void ErrorTitleEmpty(string? title)
+    {
+        var validator = new CreateExpenseValidator();
+        var request = RequestExpenseCreateJsonBuilder.Build();
+        request.Title = title;
+
+        var result = validator.Validate(request);
+
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+
+    [Fact]
+    public void ErrorDateFuture()
+    {
+        var validator = new CreateExpenseValidator();
+        var request = RequestExpenseCreateJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        var result = validator.Validate(request);
+
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_BE_FUTURE));
+    }
+
+    [Fact]
+    public void ErrorPaymentTypeInvalid()
+    {
+        var validator = new CreateExpenseValidator();
+        var request = RequestExpenseCreateJsonBuilder.Build();
+        request.PaymentType = (PaymentType)55;
+
+        var result = validator.Validate(request);
+
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+
+    [Theory]
+    [InlineData(-7)]
+    [InlineData(0)]
+    public void ErrorPriceInvalid(decimal price)
+    {
+        var validator = new CreateExpenseValidator();
+        var request = RequestExpenseCreateJsonBuilder.Build();
+        request.Price = price;
+
+        var result = validator.Validate(request);
+
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage.Equals(ResourceErrorMessages.PRICE_GREATER_THAN_ZERO));
+    }
+
+    public static IEnumerable<object[]> GetWrongTitleData()
+    {
+        yield return new object[] { "" };
+        yield return new object[] { "  " };
+        yield return new object[] { null };
     }
 }
