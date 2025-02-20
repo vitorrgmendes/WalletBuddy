909d06765a26892c035a47e0d3cc26ba25efaf32
Vitor Mendes <vitormendes20@gmail.com>
2025-02-20 18:03:41 -0300

    feat: add expense entity

---
+++ src/WalletBuddy.Domain/Entities/Expense.cs
@@ -0,0 +1,13 @@
+﻿using WalletBuddy.Domain.Enums;
+
+namespace WalletBuddy.Domain.Entities;
+
+public class Expense
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Price { get; set; }
+    public PaymentType PaymentType { get; set; }
+}
---
+++ src/WalletBuddy.Domain/Enums/PaymentType.cs
@@ -0,0 +1,9 @@
+﻿namespace WalletBuddy.Domain.Enums;
+
+public enum PaymentType
+{
+    Cash = 0,
+    CreditCard = 1,
+    DebitCard = 2,
+    EletronicTransfer = 3,
+}
